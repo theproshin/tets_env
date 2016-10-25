3e118973d50ac926e583ea455db753a2ee3066b4
theproshin <theproshin@thr.local>
2016-10-25 23:45:46 +0300

    Add Progressbar

--- test_env.py
+++ test_env.py
@@ -8,6 +8,7 @@ import sys
 import zipfile
 import argparse
 from time import sleep
+import threading
 
 """
 Setup environment for run test
@@ -37,7 +38,8 @@ def head():
 def svn_checkout(url, path, user, password):
     """Call svn checkout"""
 
-    exit_code = subprocess.Popen(['svn', 'co', url, path, '--username', user, '--password', password]).wait()
+    exit_code = subprocess.Popen(['svn', 'co', url, path, '--username', user, '--password', password],
+                                 stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).wait()
     if exit_code != 0:
         raise Exception('Don\'t get data from repository')
 
@@ -54,6 +56,7 @@ def get_chromedriver(ver_chromedriver):
     win_filename = 'chromedriver_win32.zip'
     url_chromedriver = 'http://chromedriver.storage.googleapis.com/{}/{}'.format(ver_chromedriver, win_filename)
     download_file(url_chromedriver, win_filename)
+    return True
 
 
 def get_standalone_server(ver_standalone_server):
@@ -65,11 +68,13 @@ def get_standalone_server(ver_standalone_server):
     url_selenium_server = 'http://selenium-release.storage.googleapis.com/{}/{}'.format(ver_standalone_server,
                                                                                         file_standalone_server)
     download_file(url_selenium_server, file_standalone_server)
+    return True
 
 
 def get_atf(ver_atf, user, password):
     """Get automated testing framework"""
 
+    return True
     url = 'https://svn.sbis.ru/svn/sbis3-tests/atf/release/{}/'.format(ver_atf)
     path = os.path.join(TMP_FOLDER, 'atf')
     if os.path.exists(path):
@@ -80,6 +85,7 @@ def get_atf(ver_atf, user, password):
 def get_genie_tests(ver_genie, user, password):
     """Get test from svn"""
 
+    return True
     url = 'https://svn.sbis.ru/svn/sbis3-tests/genie/{}/'.format(ver_genie)
     path = os.path.join(TMP_FOLDER, 'tests')
     if os.path.exists(path):
@@ -91,15 +97,18 @@ def install_lib(user, password):
     """Install lib's through pip"""
 
     os.environ['HTTP_PROXY'] = 'http://{}:{}@ias.corp.tensor.ru:8080'.format(user, password)
-    p = subprocess.Popen(['pip', 'install', '-r', 'requirement.txt', '-U', '--disable-pip-version-check'])
+    p = subprocess.Popen(['pip', 'install', '-r', 'requirement.txt', '-U', '--disable-pip-version-check'],
+                         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     exit_code = p.wait()
     if exit_code != 0:
         raise Exception('Don\'t install packet from pip')
+    return True
 
 
 def copy_into_site_packages():
     """Copy atf, PO into folder site-packages"""
 
+    return True
     site_packages = [path for path in sys.path if 'site-packages' in path][0]
     try:
         shutil.copytree(os.path.join(TMP_FOLDER, 'atf'), os.path.join(site_packages, 'atf'))
@@ -114,6 +123,7 @@ def copy_chromedriver():
 
     exec_dir = sys.prefix
     zipfile.ZipFile(os.path.join(TMP_FOLDER, 'chromedriver_win32.zip')).extractall(path=os.path.join(exec_dir, 'bin'))
+    return True
 
 
 def add_arguments():
@@ -130,50 +140,74 @@ def add_arguments():
     return parg
 
 
-def progressbar():
-    """View progressbar"""
+class ProgressBar(threading.Thread):
+    """Override thread"""
 
-    ends = 50
-
-    for i in range(ends):
-        sys.stdout.write(u'\001b[100D' + '%s%%' % (i + 1))
-        sys.stdout.flush()
-        sleep(0.1)
-        if i == ends-1:
-            sys.stdout.write('u\001b[100D' + '[ OK ]')
+    def run(self):
+        global stop
+        while not stop:
+            sys.stdout.write(u'\u001b[100D \u001b[50C \u001b[31m' + '{:^20}'.format('[ WAIT ]') + u'\u001b[0m')
             sys.stdout.flush()
+            sleep(0.2)
+        else:
+            sys.stdout.write(u'\u001b[100D \u001b[50C \u001b[32m' + '{:^20}'.format('[ OK ]') + u'\u001b[0m')
+            sys.stdout.flush()
+            sys.stdout.write('\n')
 
 
-def main():
-    """Common algorithm"""
+if __name__ == '__main__':
+    head()
     args = add_arguments()
     p = args.parse_args()
     if not os.path.exists(TMP_FOLDER):
         print('[*] create tmp folder: ', TMP_FOLDER)
         os.mkdir(TMP_FOLDER)
 
-    print('[*] get chromedriver..')
-    get_chromedriver(p.cd)
+    stop = False
+    print(' [*] get chromedriver..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = get_chromedriver(p.cd)
+    pb.join()
+    stop = False
 
-    print('[*] get selenium-standalone-server..')
-    get_standalone_server(p.ss)
+    print(' [*] get selenium-standalone-server..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = get_standalone_server(p.ss)
+    pb.join()
+    stop = False
 
-    print('[*] get atf..')
-    get_atf(p.atf, p.u, p.p)
+    print(' [*] get atf..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = get_atf(p.atf, p.u, p.p)
+    pb.join()
+    stop = False
 
-    print('[*] get tests..')
-    get_genie_tests(p.wg, p.u, p.p)
+    print(' [*] get tests..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = get_genie_tests(p.wg, p.u, p.p)
+    pb.join()
+    stop = False
 
-    print('[*] install lib..')
-    install_lib(p.u, p.p)
+    print(' [*] install lib..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = install_lib(p.u, p.p)
+    pb.join()
+    stop = False
 
-    print('[*] copy data into site-packages..')
-    copy_into_site_packages()
+    print(' [*] copy data into site-packages..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = copy_into_site_packages()
+    pb.join()
+    stop = False
 
-    print('[*] copy chromedriver..')
-    copy_chromedriver()
-
-
-if __name__ == '__main__':
-    head()
-    main()
+    print(' [*] copy chromedriver..', end='')
+    pb = ProgressBar()
+    pb.start()
+    stop = copy_chromedriver()
+    pb.join()
